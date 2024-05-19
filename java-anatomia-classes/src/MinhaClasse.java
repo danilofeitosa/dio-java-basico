d52361f2143d3a88cac8c078995e50b4c9a94429
Danilo <danilo.feitosa94@hotmail.com>
2024-05-19 20:46:55 -0300

    declarando-variaveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,17 @@ public class MinhaClasse {
 
 public static void main (String [] args) {
 
+    String primeiroNome = "Danilo";
+    String segundoNome = "Rodrigues";
 
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 }
     
 }
